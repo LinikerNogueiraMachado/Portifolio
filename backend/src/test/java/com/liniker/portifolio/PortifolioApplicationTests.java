d9cf1e0e9c2372bb744530155022fb44c7bcf09e
Liniker Nogueira Machado <linikernogueiramachado@gmail.com>
2022-07-12 21:10:29 -0300

    Início de Projeto

---
+++ backend/src/test/java/com/liniker/portifolio/PortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liniker.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
